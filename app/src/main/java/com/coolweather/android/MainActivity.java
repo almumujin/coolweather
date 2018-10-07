047640546dc43d45a51aff4b18891d1bc434412b
almumujin <excalibur_jin@163.com>
2018-10-07 09:45:51 +0800

    加入创建数据库和表的各项配置.

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -3,6 +3,8 @@ package com.coolweather.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.hasee.coolweather.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,35 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
+
