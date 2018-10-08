11f7f44003d1c0fdca18619f77f926048b35504d
almumujin <excalibur_jin@163.com>
2018-10-08 16:08:33 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -3,7 +3,7 @@ package com.coolweather.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.hasee.coolweather.R;
+import com.coolweather.android.R;
 
 public class MainActivity extends AppCompatActivity {
 
